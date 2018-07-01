6475c2da600766511e21417f1214821d37c34ae1
monkeydkon <kon_fu2@hotmail.com>
2018-07-01 19:52:38 +0300

    added bubblesort

--- Algorithms/Sorting Algorithms/Bubblesort/src/monkeydkon/com/nplab/Main.java
+++ Algorithms/Sorting Algorithms/Bubblesort/src/monkeydkon/com/nplab/Main.java
@@ -3,6 +3,32 @@ package monkeydkon.com.nplab;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int [] intArray = {20, 35, -15, 7 ,55, 1, -22, -10, 30, 12312312, -3, 0, -1, 3, -123, 1234122412, -3, 12};
+
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--){
+            for (int i = 0; i < lastUnsortedIndex; i++){
+                if( intArray[i] > intArray[i + 1]){
+                    swap(intArray, i, i+1);
+                }
+            }
+        }
+
+        for (int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+
+    }
+
+    public static void swap(int[] array, int i, int j){
+
+        if (i == j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
     }
 }
